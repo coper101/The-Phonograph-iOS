ad79cdbda6775ab7481e177ee488f5d45a1d2be0
Daryl <darylgialolo@gmail.com>
2022-07-11 19:16:49 +0800

    Initial Commit

---
+++ The Phonograph/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The Phonograph
+//
+//  Created by Wind Versi on 11/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The Phonograph/The_PhonographApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_PhonographApp.swift
+//  The Phonograph
+//
+//  Created by Wind Versi on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct The_PhonographApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
